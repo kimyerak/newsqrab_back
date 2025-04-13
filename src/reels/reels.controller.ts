6906cd3573f0981bf62d3629ea69688da8532a0e
syeongkim <carsk0607@gmail.com>
2025-04-14 02:05:34 +0900

    fix

--- src/reels/reels.controller.ts
+++ src/reels/reels.controller.ts
@@ -10,98 +10,98 @@ import { CommentDto } from './dto/comment.dto';
 export class ReelsController {
   constructor(private readonly reelsService: ReelsService) {}
 
-  // @Post()
-  // @ApiOperation({ summary: '프론트에서 쓸일 x - Create a new reels' })
-  // @ApiResponse({
-  //   status: 201,
-  //   description: 'The reels has been successfully created.',
-  //   type: Reels,
-  // })
-  // create(@Body() createReelsDto: CreateReelsDto): Promise<Reels> {
-  //   return this.reelsService.create(createReelsDto);
-  // }
+  @Post()
+  @ApiOperation({ summary: '프론트에서 쓸일 x - Create a new reels' })
+  @ApiResponse({
+    status: 201,
+    description: 'The reels has been successfully created.',
+    type: Reels,
+  })
+  create(@Body() createReelsDto: CreateReelsDto): Promise<Reels> {
+    return this.reelsService.create(createReelsDto);
+  }
 
-  // @Put(':id')
-  // @ApiOperation({ summary: '프론트에선 x - Update reels information' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'The reels information has been successfully updated.',
-  //   type: Reels,
-  // })
-  // @ApiResponse({ status: 404, description: 'Reels not found.' })
-  // update(
-  //   @Param('id') id: string,
-  //   @Body() updateReelsDto: UpdateReelsDto,
-  // ): Promise<Reels> {
-  //   return this.reelsService.update(id, updateReelsDto);
-  // }
+  @Put(':id')
+  @ApiOperation({ summary: '프론트에선 x - Update reels information' })
+  @ApiResponse({
+    status: 200,
+    description: 'The reels information has been successfully updated.',
+    type: Reels,
+  })
+  @ApiResponse({ status: 404, description: 'Reels not found.' })
+  update(
+    @Param('id') id: string,
+    @Body() updateReelsDto: UpdateReelsDto,
+  ): Promise<Reels> {
+    return this.reelsService.update(id, updateReelsDto);
+  }
 
-  // @Get(':date')
-  // @ApiOperation({ summary: '탭4 - Get reels by date' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'List of reels for the specified date.',
-  //   type: [Reels],
-  // })
-  // @ApiResponse({
-  //   status: 404,
-  //   description: 'No reels found for the specified date.',
-  // })
-  // getByDate(@Param('date') date: string): Promise<Reels[]> {
-  //   return this.reelsService.findByDate(date);
-  // }
+  @Get(':date')
+  @ApiOperation({ summary: '탭4 - Get reels by date' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of reels for the specified date.',
+    type: [Reels],
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No reels found for the specified date.',
+  })
+  getByDate(@Param('date') date: string): Promise<Reels[]> {
+    return this.reelsService.findByDate(date);
+  }
 
-  // @Get('owner/:owner')
-  // @ApiOperation({ summary: '특정 소유자별로 릴스 가져오기' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'List of reels for the specified owner.',
-  //   type: [Reels],
-  // })
-  // @ApiResponse({
-  //   status: 404,
-  //   description: 'No reels found for the specified owner.',
-  // })
-  // getByOwner(@Param('owner') owner: string): Promise<Reels[]> {
-  //   return this.reelsService.findByOwner(owner);
-  // }
+  @Get('owner/:owner')
+  @ApiOperation({ summary: '특정 소유자별로 릴스 가져오기' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of reels for the specified owner.',
+    type: [Reels],
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No reels found for the specified owner.',
+  })
+  getByOwner(@Param('owner') owner: string): Promise<Reels[]> {
+    return this.reelsService.findByOwner(owner);
+  }
 
-  // @Get(':id/comments/sorted')
-  // @ApiOperation({ summary: 'Get comments sorted by likes' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'Comments sorted by likes.',
-  //   type: [CommentDto],
-  // })
-  // async getCommentsSorted(@Param('id') id: string): Promise<CommentDto[]> {
-  //   return this.reelsService.getCommentsSorted(id);
-  // }
+  @Get(':id/comments/sorted')
+  @ApiOperation({ summary: 'Get comments sorted by likes' })
+  @ApiResponse({
+    status: 200,
+    description: 'Comments sorted by likes.',
+    type: [CommentDto],
+  })
+  async getCommentsSorted(@Param('id') id: string): Promise<CommentDto[]> {
+    return this.reelsService.getCommentsSorted(id);
+  }
 
-  // @Put(':id/comments')
-  // @ApiOperation({ summary: 'Add a comment to a reel' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'Comment added successfully.',
-  //   type: Reels,
-  // })
-  // async addComment(
-  //   @Param('id') id: string,
-  //   @Body() commentDto: CommentDto,
-  // ): Promise<Reels> {
-  //   return this.reelsService.addComment(id, commentDto);
-  // }
+  @Put(':id/comments')
+  @ApiOperation({ summary: 'Add a comment to a reel' })
+  @ApiResponse({
+    status: 200,
+    description: 'Comment added successfully.',
+    type: Reels,
+  })
+  async addComment(
+    @Param('id') id: string,
+    @Body() commentDto: CommentDto,
+  ): Promise<Reels> {
+    return this.reelsService.addComment(id, commentDto);
+  }
 
-  // @Put(':id/comments/:commentId/like')
-  // @ApiOperation({ summary: 'Add a like to a comment' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'Like added successfully.',
-  //   type: Reels,
-  // })
-  // async likeComment(
-  //   @Param('id') id: string,
-  //   @Param('commentId') commentId: string,
-  // ): Promise<Reels> {
-  //   return this.reelsService.likeComment(id, commentId);
-  // }
+  @Put(':id/comments/:commentId/like')
+  @ApiOperation({ summary: 'Add a like to a comment' })
+  @ApiResponse({
+    status: 200,
+    description: 'Like added successfully.',
+    type: Reels,
+  })
+  async likeComment(
+    @Param('id') id: string,
+    @Param('commentId') commentId: string,
+  ): Promise<Reels> {
+    return this.reelsService.likeComment(id, commentId);
+  }
 }
--- src/reels/reels.service.ts
+++ src/reels/reels.service.ts
@@ -23,224 +23,224 @@ export class ReelsService {
     private readonly s3Service: S3Service,
   ) {}
 
-  // async create(createReelsDto: CreateReelsDto): Promise<Reels> {
-  //   // 기본 owner 값 설정
-  //   const owner = createReelsDto.owner || 'newsqrap';
+  async create(createReelsDto: CreateReelsDto): Promise<Reels> {
+    // 기본 owner 값 설정
+    const owner = createReelsDto.owner || 'newsqrap';
 
-  //   const newReels = new this.reelsModel({
-  //     ...createReelsDto,
-  //     owner,
-  //   });
-  //   return newReels.save();
-  // }
+    const newReels = new this.reelsModel({
+      ...createReelsDto,
+      owner,
+    });
+    return newReels.save();
+  }
 
-  // async update(id: string, updateReelsDto: UpdateReelsDto): Promise<Reels> {
-  //   const updatedReels = await this.reelsModel
-  //     .findByIdAndUpdate(id, updateReelsDto, { new: true })
-  //     .exec();
-  //   if (!updatedReels) {
-  //     throw new NotFoundException(`Reels with ID ${id} not found`);
-  //   }
-  //   return updatedReels;
-  // }
-  // async findByDate(date: string): Promise<Reels[]> {
-  //   // Assuming the 'createdAt' field exists and is in ISO format
-  //   const start = new Date(date);
-  //   start.setUTCHours(0, 0, 0, 0);
-  //   const end = new Date(date);
-  //   end.setUTCHours(23, 59, 59, 999);
+  async update(id: string, updateReelsDto: UpdateReelsDto): Promise<Reels> {
+    const updatedReels = await this.reelsModel
+      .findByIdAndUpdate(id, updateReelsDto, { new: true })
+      .exec();
+    if (!updatedReels) {
+      throw new NotFoundException(`Reels with ID ${id} not found`);
+    }
+    return updatedReels;
+  }
+  async findByDate(date: string): Promise<Reels[]> {
+    // Assuming the 'createdAt' field exists and is in ISO format
+    const start = new Date(date);
+    start.setUTCHours(0, 0, 0, 0);
+    const end = new Date(date);
+    end.setUTCHours(23, 59, 59, 999);
 
-  //   return this.reelsModel
-  //     .find({
-  //       createdAt: {
-  //         $gte: start,
-  //         $lte: end,
-  //       },
-  //     })
-  //     .exec();
-  // }
+    return this.reelsModel
+      .find({
+        createdAt: {
+          $gte: start,
+          $lte: end,
+        },
+      })
+      .exec();
+  }
 
-  // async createReelFromArticle(article: Article): Promise<void> {
-  //   // console.log('l', article.category);
-  //   const createReelsDto = new CreateReelsDto();
-  //   createReelsDto.owner = 'newsqrap';
-  //   createReelsDto.articleId = [article._id as Types.ObjectId];
-  //   createReelsDto.speak = await this.createAudioFromText(
-  //     article.summary,
-  //     createReelsDto.articleId,
-  //   ); // tts mp3 파일의 object storage url
-  //   createReelsDto.category = article.category;
-  //   const reelsPath = await this.mergeVideoAndAudio(
-  //     article.category,
-  //     createReelsDto.articleId,
-  //   );
-  //   const reelsUrl = await this.uploadFileToStorage(reelsPath);
-  //   createReelsDto.video = reelsUrl;
-  //   const newReels = new this.reelsModel(createReelsDto);
-  //   await newReels.save();
-  // }
+  async createReelFromArticle(article: Article): Promise<void> {
+    // console.log('l', article.category);
+    const createReelsDto = new CreateReelsDto();
+    createReelsDto.owner = 'newsqrap';
+    createReelsDto.articleId = [article._id as Types.ObjectId];
+    createReelsDto.speak = await this.createAudioFromText(
+      article.summary,
+      createReelsDto.articleId,
+    ); // tts mp3 파일의 object storage url
+    createReelsDto.category = article.category;
+    const reelsPath = await this.mergeVideoAndAudio(
+      article.category,
+      createReelsDto.articleId,
+    );
+    const reelsUrl = await this.uploadFileToStorage(reelsPath);
+    createReelsDto.video = reelsUrl;
+    const newReels = new this.reelsModel(createReelsDto);
+    await newReels.save();
+  }
 
-  // async createAudioFromText(
-  //   summary: string,
-  //   reelsId: Types.ObjectId[],
-  // ): Promise<string> {
-  //   const clovaspeech_url =
-  //     'https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts';
-  //   console.log(summary);
-  //   const options = {
-  //     method: 'post',
-  //     url: clovaspeech_url,
-  //     data: qs.stringify({
-  //       speaker: 'nara',
-  //       volume: '0',
-  //       speed: '-1',
-  //       pitch: '0',
-  //       text: summary,
-  //       format: 'mp3',
-  //     }),
-  //     headers: {
-  //       'X-NCP-APIGW-API-KEY-ID': process.env.X_NCP_APIGW_API_KEY_ID,
-  //       'X-NCP-APIGW-API-KEY': process.env.X_NCP_APIGW_API_KEY,
-  //       'Content-Type': 'application/x-www-form-urlencoded',
-  //     },
-  //     responseType: 'stream',
-  //   };
-  //   try {
-  //     console.log('try문 들어옴#################');
-  //     const response = await axios(options);
-  //     console.log('axios반응 받음#################');
-  //     const filePath = `./assets/tts/${reelsId}.mp3`;
-  //     console.log('저장경로설정함#################');
-  //     const writer = fs.createWriteStream(filePath);
-  //     console.log('fs로 저장#################');
-  //     response.data.pipe(writer);
-  //     console.log('pipe 작동#################');
-  //     return new Promise((resolve, reject) => {
-  //       writer.on('finish', () => {
-  //         console.log('파일 쓰기 완료');
-  //         resolve(filePath);
-  //       });
-  //       writer.on('error', reject);
-  //     });
-  //   } catch (error) {
-  //     console.error('Error fetching TTS:', error);
-  //     // throw error;
-  //     return 'default-path.mp3';
-  //   }
-  // }
+  async createAudioFromText(
+    summary: string,
+    reelsId: Types.ObjectId[],
+  ): Promise<string> {
+    const clovaspeech_url =
+      'https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts';
+    console.log(summary);
+    const options = {
+      method: 'post',
+      url: clovaspeech_url,
+      data: qs.stringify({
+        speaker: 'nara',
+        volume: '0',
+        speed: '-1',
+        pitch: '0',
+        text: summary,
+        format: 'mp3',
+      }),
+      headers: {
+        'X-NCP-APIGW-API-KEY-ID': process.env.X_NCP_APIGW_API_KEY_ID,
+        'X-NCP-APIGW-API-KEY': process.env.X_NCP_APIGW_API_KEY,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      responseType: 'stream',
+    };
+    try {
+      console.log('try문 들어옴#################');
+      const response = await axios(options);
+      console.log('axios반응 받음#################');
+      const filePath = `./assets/tts/${reelsId}.mp3`;
+      console.log('저장경로설정함#################');
+      const writer = fs.createWriteStream(filePath);
+      console.log('fs로 저장#################');
+      response.data.pipe(writer);
+      console.log('pipe 작동#################');
+      return new Promise((resolve, reject) => {
+        writer.on('finish', () => {
+          console.log('파일 쓰기 완료');
+          resolve(filePath);
+        });
+        writer.on('error', reject);
+      });
+    } catch (error) {
+      console.error('Error fetching TTS:', error);
+      // throw error;
+      return 'default-path.mp3';
+    }
+  }
 
-  // async mergeVideoAndAudio(
-  //   category: string,
-  //   reelsId: Types.ObjectId[],
-  // ): Promise<string> {
-  //   ffmpeg.setFfmpegPath(ffmpegPath);
-  //   const videoInputPath = `./assets/video/${category}.mp4`;
-  //   const audioInputPath = `./assets/tts/${reelsId}.mp3`;
-  //   const outputPath = `./assets/reels/${reelsId}.mp4`;
+  async mergeVideoAndAudio(
+    category: string,
+    reelsId: Types.ObjectId[],
+  ): Promise<string> {
+    ffmpeg.setFfmpegPath(ffmpegPath);
+    const videoInputPath = `./assets/video/${category}.mp4`;
+    const audioInputPath = `./assets/tts/${reelsId}.mp3`;
+    const outputPath = `./assets/reels/${reelsId}.mp4`;
 
-  //   try {
-  //     return new Promise((resolve, reject) => {
-  //       ffmpeg(videoInputPath)
-  //         .input(audioInputPath)
-  //         .outputOptions([
-  //           '-map 0:v', // 비디오 파일의 비디오 스트림을 선택
-  //           '-map 1:a', // 오디오 파일의 오디오 스트림을 선택
-  //           '-c:v copy', // 비디오 코덱은 복사 (재인코딩 없음)
-  //           '-c:a aac', // 오디오 코덱은 AAC로 설정
-  //           '-strict experimental', // 일부 코덱에 필요한 경우
-  //           '-shortest',
-  //         ])
-  //         .on('error', function (err) {
-  //           console.log('An error occurred during merging: ' + err.message);
-  //           reject(err);
-  //         })
-  //         .on('end', function () {
-  //           console.log('Merging finished successfully!');
-  //           resolve(outputPath);
-  //         })
-  //         .save(outputPath);
-  //     });
-  //   } catch (error) {
-  //     console.error('Error during video and audio merging:', error);
-  //     // throw new Error('Merging process failed.');
-  //     return 'default-path.mp3';
-  //   }
-  // }
-  // async uploadFileToStorage(filePath: string): Promise<string> {
-  //   try {
-  //     const fileBuffer = fs.readFileSync(filePath);
-  //     const mimeType = 'video/mp4'; // Set MIME type for video
-  //     const fakeFile: Express.Multer.File = {
-  //       buffer: fileBuffer,
-  //       originalname: path.basename(filePath),
-  //       mimetype: mimeType,
-  //       fieldname: 'file', // Typically the name of the form field (used in Multer)
-  //       encoding: '7bit', // Common encoding for files
-  //       size: fileBuffer.length,
-  //       destination: '', // Optional: the folder to which the file has been saved
-  //       filename: path.basename(filePath), // The name of the file within the destination
-  //       path: filePath, // The full path to the uploaded file
-  //       stream: fs.createReadStream(filePath), // A readable stream of the file
-  //     };
+    try {
+      return new Promise((resolve, reject) => {
+        ffmpeg(videoInputPath)
+          .input(audioInputPath)
+          .outputOptions([
+            '-map 0:v', // 비디오 파일의 비디오 스트림을 선택
+            '-map 1:a', // 오디오 파일의 오디오 스트림을 선택
+            '-c:v copy', // 비디오 코덱은 복사 (재인코딩 없음)
+            '-c:a aac', // 오디오 코덱은 AAC로 설정
+            '-strict experimental', // 일부 코덱에 필요한 경우
+            '-shortest',
+          ])
+          .on('error', function (err) {
+            console.log('An error occurred during merging: ' + err.message);
+            reject(err);
+          })
+          .on('end', function () {
+            console.log('Merging finished successfully!');
+            resolve(outputPath);
+          })
+          .save(outputPath);
+      });
+    } catch (error) {
+      console.error('Error during video and audio merging:', error);
+      // throw new Error('Merging process failed.');
+      return 'default-path.mp3';
+    }
+  }
+  async uploadFileToStorage(filePath: string): Promise<string> {
+    try {
+      const fileBuffer = fs.readFileSync(filePath);
+      const mimeType = 'video/mp4'; // Set MIME type for video
+      const fakeFile: Express.Multer.File = {
+        buffer: fileBuffer,
+        originalname: path.basename(filePath),
+        mimetype: mimeType,
+        fieldname: 'file', // Typically the name of the form field (used in Multer)
+        encoding: '7bit', // Common encoding for files
+        size: fileBuffer.length,
+        destination: '', // Optional: the folder to which the file has been saved
+        filename: path.basename(filePath), // The name of the file within the destination
+        path: filePath, // The full path to the uploaded file
+        stream: fs.createReadStream(filePath), // A readable stream of the file
+      };
 
-  //     // Assuming `uploadFile` is properly configured to take an object similar to `Express.Multer.File`
-  //     return await this.s3Service.uploadFile('reels', fakeFile);
-  //   } catch (error) {
-  //     console.error('Error uploading file to storage:', error);
-  //     // throw new Error('File upload failed.');
-  //     return 'default-path.mp3';
-  //   }
-  // }
-  // async findByOwner(owner: string): Promise<Reels[]> {
-  //   let reels = await this.reelsModel.find({ owner }).exec();
-  //   if (!reels || reels.length === 0) {
-  //     console.log(
-  //       `No reels found for owner '${owner}'. Displaying default reels for 'newsqrap'.`,
-  //     );
-  //     reels = await this.reelsModel.find({ owner: 'newsqrap' }).exec();
-  //   }
-  //   return reels;
-  // }
-  // async getCommentsSorted(reelId: string): Promise<CommentDto[]> {
-  //   const reel = await this.reelsModel.findById(reelId).exec();
-  //   if (!reel) {
-  //     throw new NotFoundException('Reel not found');
-  //   }
-  //   const sortedComments = reel.comments.sort((a, b) => b.likes - a.likes);
-  //   return sortedComments.map((comment) => ({
-  //     _id: comment._id,
-  //     userId: comment.userId,
-  //     nickname: comment.nickname,
-  //     profilePicture: comment.profilePicture,
-  //     content: comment.content,
-  //     likes: comment.likes,
-  //   }));
-  // }
+      // Assuming `uploadFile` is properly configured to take an object similar to `Express.Multer.File`
+      return await this.s3Service.uploadFile('reels', fakeFile);
+    } catch (error) {
+      console.error('Error uploading file to storage:', error);
+      // throw new Error('File upload failed.');
+      return 'default-path.mp3';
+    }
+  }
+  async findByOwner(owner: string): Promise<Reels[]> {
+    let reels = await this.reelsModel.find({ owner }).exec();
+    if (!reels || reels.length === 0) {
+      console.log(
+        `No reels found for owner '${owner}'. Displaying default reels for 'newsqrap'.`,
+      );
+      reels = await this.reelsModel.find({ owner: 'newsqrap' }).exec();
+    }
+    return reels;
+  }
+  async getCommentsSorted(reelId: string): Promise<CommentDto[]> {
+    const reel = await this.reelsModel.findById(reelId).exec();
+    if (!reel) {
+      throw new NotFoundException('Reel not found');
+    }
+    const sortedComments = reel.comments.sort((a, b) => b.likes - a.likes);
+    return sortedComments.map((comment) => ({
+      _id: comment._id,
+      userId: comment.userId,
+      nickname: comment.nickname,
+      profilePicture: comment.profilePicture,
+      content: comment.content,
+      likes: comment.likes,
+    }));
+  }
 
-  // async addComment(reelId: string, commentDto: CommentDto): Promise<Reels> {
-  //   const reel = await this.reelsModel.findById(reelId).exec();
-  //   if (!reel) {
-  //     throw new NotFoundException('Reel not found');
-  //   }
-  //   reel.comments.push(commentDto);
-  //   return reel.save();
-  // }
+  async addComment(reelId: string, commentDto: CommentDto): Promise<Reels> {
+    const reel = await this.reelsModel.findById(reelId).exec();
+    if (!reel) {
+      throw new NotFoundException('Reel not found');
+    }
+    reel.comments.push(commentDto);
+    return reel.save();
+  }
 
-  // async likeComment(reelId: string, commentId: string): Promise<Reels> {
-  //   const reel = await this.reelsModel.findById(reelId).exec();
-  //   if (!reel) {
-  //     throw new NotFoundException('Reel not found');
-  //   }
+  async likeComment(reelId: string, commentId: string): Promise<Reels> {
+    const reel = await this.reelsModel.findById(reelId).exec();
+    if (!reel) {
+      throw new NotFoundException('Reel not found');
+    }
 
-  //   // commentId를 ObjectId로 변환
-  //   const objectId = new Types.ObjectId(commentId);
+    // commentId를 ObjectId로 변환
+    const objectId = new Types.ObjectId(commentId);
 
-  //   // 서브도큐먼트의 id로 찾기
-  //   const comment = reel.comments.id(objectId);
-  //   if (!comment) {
-  //     throw new NotFoundException('Comment not found');
-  //   }
-  //   comment.likes += 1;
-  //   return reel.save();
-  // }
+    // 서브도큐먼트의 id로 찾기
+    const comment = reel.comments.id(objectId);
+    if (!comment) {
+      throw new NotFoundException('Comment not found');
+    }
+    comment.likes += 1;
+    return reel.save();
+  }
 }
