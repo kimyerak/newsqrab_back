2345adbc0b5790b49c8295413426c913e610fcdc
syeongkim <carsk0607@gmail.com>
2025-05-05 02:17:49 +0900

    feat: add tts api

--- src/reels/reels.module.ts
+++ src/reels/reels.module.ts
@@ -6,11 +6,13 @@ import { ReelsController } from './reels.controller';
 import { Article, ArticleSchema } from '../article/article.schema';
 import { OpenAiService } from '../openai/openai.service';
 import { S3Module } from '../s3/s3.module';
+import { Conversation, ConversationSchema } from '../conversation/conversation.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Reels.name, schema: ReelsSchema }]),
     MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }]),
+    MongooseModule.forFeature([{ name: Conversation.name, schema: ConversationSchema}]),
     S3Module,
   ],
   controllers: [ReelsController],
