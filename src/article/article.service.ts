b0cd3a78f2221d0da7108515607bffc659bba66b
syeongkim <carsk0607@gmail.com>
2025-07-09 23:37:35 +0900

    feat: 영상에 이미지 합성

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -6,19 +6,39 @@ import { CreateArticleDto } from './dto/create-article.dto';
 import { crawlNaverNewsContent } from '../utils/naver-crawler';
 import { ReelsService } from '../reels/reels.service';
 import { ConversationService } from '../conversation/conversation.service';
+import axios from 'axios';
+import { S3Service } from '../s3/s3.service';
+
 @Injectable()
 export class ArticleService {
   constructor(
     @InjectModel(Article.name) private articleModel: Model<Article>,
     private readonly reelsService: ReelsService,
     private readonly conversationService: ConversationService,
+    private readonly s3Service: S3Service,
   ) {}
 
   async create(createArticleDto: CreateArticleDto): Promise<Article> {
     const { url } = createArticleDto;
     const data = await crawlNaverNewsContent(createArticleDto.url);
     const content = data.content;
-    const imgurl = data.imgurl;
+    const articleImageUrl = data.imgurl;
+
+    // 이미지 다운로드
+    const imageResponse = await axios.get(articleImageUrl, {
+      responseType: 'arraybuffer',
+    });
+    const buffer = Buffer.from(imageResponse.data, 'binary');
+
+    // S3 업로드
+    const imgurl = await this.s3Service.uploadBuffer(
+      'newscrab-thumbnails',
+      buffer,
+      'articleImage.png',
+      imageResponse.headers['content-type'],
+    );
+
+    // console.log(uploadedUrl);
 
     const newArticle = new this.articleModel({
       url,
